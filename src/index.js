fee47f70c737ea5b346eb83b486c5b12c97988c9
Polarpop <doug.schamberg@gmail.com>
2019-08-08 11:55:20 -0700

    Added docs

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import { Manager as Browser } from './Browser';
 import { Manager as Pages } from './Page';
 
-
+/** @class Scrappy */
 exports.Scrappy = class Scrappy {
   /**
   * Creates the browser manager instance. Which has the property of 
